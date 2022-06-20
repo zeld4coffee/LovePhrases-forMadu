68719810029d22589179c699aa59f86d517e02b6
Ailton cesar <ailtonc.n64@gmail.com>
2022-06-20 18:20:04 -0300

    added more phrases and fixed array run

--- app/src/main/java/com/example/frasesparamadu/MainActivity.java
+++ app/src/main/java/com/example/frasesparamadu/MainActivity.java
@@ -18,17 +18,16 @@ public class MainActivity extends AppCompatActivity {
 
     public void gerarNovaFrase(View view) {
 
-        String[] frasesParaGerar = {
-                "frase1",
-                "aaaa",
-                "bbb",
-
+        String[] frasesParaGerar = { // phrases array
+                "Eu te amo muito, não se esqueça disso <3",
+                "Você é maravilhosa <3",
+                "Confie no seu potencial!",
+                "Minha Princesa <3",
+                "Não importa se o munda acha clichê, Um dia vamos casar!",
         };
 
-        int number = new Random().nextInt(4);
-
-        TextView text = findViewById(R.id.textoResult);
-        text.setText(frasesParaGerar[number]);
-
+        int number = new Random().nextInt(4); // create a random number
+        TextView text = findViewById(R.id.textoResult); // find de textView
+        text.setText(frasesParaGerar[number]); // set the text acording array
     }
 }
